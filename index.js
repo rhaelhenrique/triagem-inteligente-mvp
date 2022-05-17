216ca6c5bb2a6dd98da52788860cd9241e70a570
Rhael Henrique <rhaelhenriquerodrigues@gmail.com>
2022-05-17 14:28:55 -0300

    main

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const { response } = require("express");
 const express = require("express");
 const fileupload = require("express-fileupload");
 const pdfparse = require("pdf-parse");
@@ -8,7 +7,7 @@ const app = express();
 app.use("/", express.static("public"));
 app.use(fileupload());
 
-app.post("/extrac-text", (req, res) =>{
+app.post("/extract-text", (req, res) =>{
     if (!req.files && !req.files.pdfFile){
         res.status(400);
         res.end();
