34a97f17b89433bb03cb7accc910b6fbe487af18
Rhael Henrique <rhaelhenriquerodrigues@gmail.com>
2022-07-14 13:38:59 -0300

    Alterações para entrega da Spring 9

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const fileupload = require("express-fileupload");
 const pdfparse = require("pdf-parse");
+const Tesseract = require ("tesseract.js");
+const { createWorker } = require('tesseract.js');
+const worker = createWorker();
 
 const app = express();
 
 app.use("/", express.static("public"));
-app.use(fileupload());
+app.use(fileupload({}));
 
+//upload pdf file
 app.post("/extract-text", (req, res) =>{
     if (!req.files && !req.files.pdfFile){
         res.status(400);
@@ -18,4 +22,20 @@ app.post("/extract-text", (req, res) =>{
     });
 });
 
-app.listen(3000);
+//upload ocr file
+app.post("/ocr-text", (req, res) =>{
+    if (!req.files && !req.files.ocrText){
+        res.status(400);
+        res.end();
+    }
+
+    Tesseract.recognize(req.files.ocrText,'eng',
+        { logger: m => console.log(m) }
+    ).then(result => {
+        res.send(result.text);
+    });
+});
+
+//service
+const PORT = 3000 || process.env.PORT;
+app.listen(PORT, () => console.log(`Usando a porta: ${PORT}`));
